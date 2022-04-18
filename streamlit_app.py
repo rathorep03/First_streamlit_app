fc0bf494762d1ed3a86dc072bc9c1cfe763550c4
rathorep03 <103935704+rathorep03@users.noreply.github.com>
2022-04-19 00:23:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
-streamlit.title('my parents new healthy diner')
+streamlit.header('my parents new healthy diner')
 streamlit.title('My Mom\'s New Healthy Dinner')
-streamlit.title('Omega 3 and Blueberry Oatmeal')
-streamlit.title('Kale, Spinach & Rocket Smoothi')
-streamlit.title('Hard-boiled Free-Range Egg')
+streamlit.text('Omega 3 and Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothi')
+streamlit.text('Hard-boiled Free-Range Egg')
