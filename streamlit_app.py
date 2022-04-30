7066d04751cc049397fe35105def1c3b657ecb7c
rathorep03 <103935704+rathorep03@users.noreply.github.com>
2022-04-30 15:04:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,19 @@ fruits_selected=streamlit.multiselect("Pick some fruits:",list(my_fruit_list.ind
 fruits_to_show=my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruityvice Fruit Advice!')
+try:
+
+fruit_choice=streamlit.text_input('what fruit would you  like information about?')
+if not fruit_choice:
+  streamlit.error("Please select a fruit to get information")
+  else:
+
+fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
+streamlit.dataframe(fruityvice_normalized)
+
+except urlError as e;
+streamlit.error()
 
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
@@ -30,16 +43,3 @@ streamlit.write('The user entered',add_my_fruit)
 my_cur.execute("insert into fruit_load_list values ('from streamlit')");
 
 from urllib.error import URLError
-try:
-
-fruit_choice=streamlit.text_input('what fruit would you  like information about?')
-if not fruit_choice:
-  streamlit.error("Please select a fruit to get information")
-  else:
-
-fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
-
-except urlError as e;
-streamlit.error()
