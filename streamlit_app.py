61bf5d2e35950a9e70fefb0d2f67221bb8011af1
rathorep03 <103935704+rathorep03@users.noreply.github.com>
2022-04-30 15:15:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,20 +16,26 @@ fruits_selected=streamlit.multiselect("Pick some fruits:",list(my_fruit_list.ind
 fruits_to_show=my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruityvice Fruit Advice!')
-try:
-
+try:` 
 fruit_choice = streamlit.text_input('what fruit would you  like information about?')
 if not fruit_choice:
   streamlit.error("Please select a fruit to get information")
   else:
-
-fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+fruityvice_response=requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 except urlError as e:
 streamlit.error()
 
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("select * from fruit_load_list")
+my_data_row = my_cur.fetchall()
+streamlit.header("The fruit load list contains:")
+streamlit.dataframe(my_data_row)
 
-
+add_my_fruit=streamlit.text_input('what fruit would you like to add?','jackfruit')
+streamlit.write('The user entered',add_my_fruit)
+my_cur.execute("insert into fruit_load_list values ('from streamlit')");
 
